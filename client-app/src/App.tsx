5bebabf98e154d714dd59d4a20b800ddc3630b38
Wesley Maffly-Kipp <wesleymafflykipp@gmail.com>
2020-04-05 18:23:39 -0500

    "end of section 3"

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,13 +1,36 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+import { Header, Icon, List } from "semantic-ui-react";
 
-const App: React.FC = () => {
-    return (
-        <div className="App">
-            <ul>Hello</ul>
-        </div>
-    );
-};
+class App extends Component {
+    state = {
+        values: [],
+    };
+
+    componentDidMount() {
+        axios.get("http://localhost:5000/api/values").then((res) => {
+            this.setState({
+                values: res.data,
+            });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Header as="h2">
+                    <Icon name="users" />
+                    <Header.Content>Reactivities</Header.Content>
+                </Header>
+                <List>
+                    {this.state.values.map((value: any) => (
+                        <List.Item key={value.id}>{value.name}</List.Item>
+                    ))}
+                </List>
+            </div>
+        );
+    }
+}
 
 export default App;
