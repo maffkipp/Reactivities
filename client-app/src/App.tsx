9c8186ab02bab5ebb4e75285eb9f3a0c5fd7ec0d
Wesley Maffly-Kipp <wesleymafflykipp@gmail.com>
2020-04-05 16:27:33 -0500

    react app initial commit

---
+++ client-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <ul>Hello</ul>
+        </div>
+    );
+};
+
+export default App;
